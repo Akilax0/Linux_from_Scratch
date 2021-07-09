51311cdf924955f0361b0b759f4dd34d299d2397
Akilax0 <killerilaksha@gmail.com>
2021-07-09 22:00:32 +0530

    scripts_init

---
+++ LFS/lfs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export LFS=/mnt/lfs
+export LFS_TGT=x86_64-lfs-linux-gnu
+#x64 lfs vendor linux
+#gnu-os
+export LFS_DISK=/dev/sda1
+#check for usb
---
+++ install.sh
@@ -0,0 +1,5 @@
+#use apt-get install for debian based
+#Packages for host machine (A)
+sudo pacman -S bash binutils bison bzip2 coreutils diffutils findutils gawk gcc 
+glibc grep gzip m4 make patch perl python3 sed tar texinfo xz
+
